ba15bb62bdd3dcce552d621435cd7f4be2d52ace
rladeon <rladeon@gmail.com>
2016-01-03 22:24:48 -0400

    add splashscreen

--- main.cpp
+++ main.cpp
@@ -26,22 +26,20 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     QImage splashScrImage ("C:/developpement/qt/greencar/asset/introd_diagnostique.png");
-    //QSize screenSize = QApplication::desktop()->geometry().size()/2;
-QSize screenSize  = splashScrImage.size()/2;
+    QSize screenSize  = splashScrImage.size()/2;
     QImage splashScr (screenSize, QImage::Format_ARGB32_Premultiplied);
     QPainter painter (&splashScr);
-    painter.fillRect(splashScr.rect(), Qt::TransparentMode);
+    painter.fillRect(splashScr.rect(), Qt::transparent);
     QImage scaled = splashScrImage.scaled(screenSize, Qt::KeepAspectRatio);
     QRect scaledRect = scaled.rect();
     scaledRect.moveCenter(splashScr.rect().center());
     painter.drawImage(scaledRect, scaled);
     QPixmap Logo;
     Logo.convertFromImage(splashScr);
-    QSplashScreen splashScrWindow (&w, Logo, Qt::WindowStaysOnTopHint);
+    QSplashScreen splashScrWindow (&w, Logo, Qt::WindowStaysOnTopHint | Qt::SplashScreen | Qt::FramelessWindowHint);
 
     splashScrWindow.move(  QApplication::desktop()->width() / 3,
-        QApplication::desktop()->height() / 3 );
-
+    QApplication::desktop()->height() / 3 );
     splashScrWindow.show();
     splashScrWindow.showMessage(QString("Loading..."),Qt::AlignCenter | Qt::AlignBottom,QColor(0,150,255));
     I::sleep(3);
